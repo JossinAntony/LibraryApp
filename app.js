a1ca08e64446b77dcefea80fa8968db738352cb6
JossinAntony <jossin.antony@causalitybiomodels.com>
2019-08-16 14:36:28 +0530

    Page title & page navbar title &links passed from app.js

--- app.js
+++ app.js
@@ -5,13 +5,25 @@ var app=new Express();
 app.set('view engine', 'ejs');
 app.use(Express.static(__dirname+"/public"));
 
+navlink=[
+    {
+        'link': '/books',
+        'title': 'Books'
+    },{
+        'link': '/authors',
+        'title': 'Authors'
+    }
+];
+
 app.get('/',(req,res)=>{
-    res.render('index');
+    res.render('index',
+    {
+        nav:navlink, title:"Library"
+    });
 });
 
 
 
-
-app.listen(3000,()=>{
+app.listen(process.env.PORT || 3000,()=>{
     console.log("Server running at http://localhost:3000")
 })
